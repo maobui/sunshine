95e99f58c5d26d01a816a62de7832c6d9cfd130f
mao.buidinh <mao.buidinh@hotmail.com>
2018-06-08 22:16:48 +0700

    - use firebaseJobDispatch to sync.

--- app/src/main/java/com/me/bui/sunshine/sync/SunshineSyncUtils.java
+++ app/src/main/java/com/me/bui/sunshine/sync/SunshineSyncUtils.java
@@ -7,7 +7,16 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
 import com.me.bui.sunshine.data.WeatherContract;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by mao.bui on 6/4/2018.
@@ -16,13 +25,38 @@ public class SunshineSyncUtils {
 
     private static boolean sInitialized;
 
+    private static final int SYNC_INTERVAL_HOURS = 3;
+    private static final int SYNC_INTERVAL_SECONDS = (int) TimeUnit.HOURS.toSeconds(SYNC_INTERVAL_HOURS);
+    private static final int SYNC_FLEXTIME_SECONDS = SYNC_INTERVAL_SECONDS / 3;
+
+    private static final String SUNSHINE_SYNC_TAG = "sunshine-sync";
+
+    static void scheduleFirebaseJobDispatcherSync(@NonNull final Context context) {
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+
+        Job syncSunshineJob = dispatcher.newJobBuilder()
+                .setService(SunshineFirebaseJobService.class)
+                .setTag(SUNSHINE_SYNC_TAG)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(SYNC_INTERVAL_SECONDS, SYNC_INTERVAL_SECONDS  + SYNC_FLEXTIME_SECONDS))
+                .setReplaceCurrent(true)
+                .build();
+
+        dispatcher.schedule(syncSunshineJob);
+    }
+
     synchronized public static void initialize(final Context context) {
         if (sInitialized) return;
         sInitialized = true;
 
-        new AsyncTask<Void, Void, Void>() {
+        scheduleFirebaseJobDispatcherSync(context);
+
+        Thread threadCheckForEmpty = new Thread(new Runnable() {
             @Override
-            protected Void doInBackground(Void... voids) {
+            public void run() {
                 Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
                 String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
                 String selectionStatement = WeatherContract.WeatherEntry
@@ -39,9 +73,9 @@ public class SunshineSyncUtils {
                     startImmediateSync(context);
                 }
                 cursor.close();
-                return null;
             }
-        }.execute();
+        });
+        threadCheckForEmpty.run();
     }
 
     public static void startImmediateSync(@NonNull final Context context) {
