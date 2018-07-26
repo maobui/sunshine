945b264aa7595a53603df6ebc8c79f2f367c4a4b
mao.buidinh <mao.buidinh@hotmail.com>
2018-07-26 11:28:34 +0700

    - create Dao.

---
+++ app/src/main/java/com/me/bui/sunshine/data/db/WeatherDao.java
@@ -0,0 +1,23 @@
+package com.me.bui.sunshine.data.db;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.Date;
+
+/**
+ * Created by mao.bui on 7/26/2018.
+ */
+
+@Dao
+public interface WeatherDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void bulkInsert(WeatherEntry ... weatherEntries);
+
+    @Query("SELECT * FROM weather WHERE date = :date")
+    WeatherEntry getWeatherByDate(Date date);
+
+}
